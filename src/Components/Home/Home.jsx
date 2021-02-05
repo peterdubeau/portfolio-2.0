bc309c4c19658984e76e26d4a28322362620cb0f
Pete Du Beau <peterdubeau@yahoo.com>
2021-02-05 16:19:08 -0500

    home page works for desktop and mobile

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import ScreenFlash from '../ScreenFlash/ScreenFlash'
 import Static from '../Static/Static'
@@ -8,12 +8,37 @@ export default function Home() {
 
   const [reset, setReset] = useState(true)
   const [power, setPower] = useState(false)
-  const [startFlash, setStartFlash] = useState({color: "white"})
+  const [startFlash, setStartFlash] = useState({})
+  const [home, setHome] = useState({
+    name: true,
+    start: true
+  })
   
   const history = useHistory()
   const slow = (ms) => {
     return new Promise(slowDown => setInterval(slowDown, ms))
   }
+
+  
+  const useMediaQuery = (query) => {
+    const [matches, setMatches] = useState(false);
+    
+    useEffect(() => {
+      const media = window.matchMedia(query);
+      if (media.matches !== matches) {
+        setMatches(media.matches);
+      }
+      const listener = () => {
+        setMatches(media.matches);
+      };
+      media.addListener(listener);
+      return () => media.removeListener(listener);
+    }, [matches, query]);
+    
+    return matches;
+  }
+  
+  let isPageWide = useMediaQuery('(max-width: 720px)')
 
   const handlePower = async () => {
     setReset(true)
@@ -22,34 +47,57 @@ export default function Home() {
   }
 
   const handleStart = async () => {
-    for (let i = 0; i < 7; i++) {
-      setStartFlash({ color: "black" })
-      await slow(100)
-      setStartFlash({ color: "white" })
-      await slow(100)
-    }
-    history.push('/menu')
-  }
+      for (let i = 0; i < 7; i++) {
+        setStartFlash({ color: "black" })
+        await slow(100)
+        setStartFlash({ color: "white" })
+        await slow(100)
+      }
+      history.push('/menu')
+    } 
 
-  const handleKeyPress = e => {
+
+  const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       handleStart()
     }
   };
 
-
-  return (
+  const gameBoyStart = async () => {
+    if (isPageWide) {
+      setHome({
+        name: true,
+        start: false
+      })
+      await slow(4000)
+      setHome({
+        name: false,
+        start: true
+      })
+    } 
+  }
+    
+  useEffect((isPageWide) => {
+      gameBoyStart()
+  }, [isPageWide])
+  
+  console.log(home)
+  
+  return (<>
     <div className='intro-container'>
+    <div className="gray-bar"></div>
       <div className='intro'>
-        <h1>Pete <br/>Du Beau</h1>
-        <h3>My Portfolio</h3>
+        {home?.name ? <h1 className="pete-dubeau">Pete Du Beau</h1> : ""}
+        {home?.start ? <>
+        <h3 className="my-portfolio">Portfolio Redux</h3>
         <br/><br/>
         <p
+          className="start"
           onClick={handleStart}
           style={startFlash}
-        >PRESS START</p>
+        >PRESS START</p></>
+       : "" }
       </div>
-    
       <div className='power-buttons'>
         <button 
           onKeyPress={handleKeyPress}
@@ -67,5 +115,5 @@ export default function Home() {
         <div className="power-light" style={power? {backgroundColor: "red"} : {backgroundColor: "black"} }></div>
       </div>
     </div>
-  )
+  </>)
 }
